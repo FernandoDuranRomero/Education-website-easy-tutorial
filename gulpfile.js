8e929c515689aff0508a8fbe491700b4d7fa5ff2
Roscko <fer@fer.com>
2022-07-26 14:11:31 -0600

    Easy-tutorial-project

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const {series, src, dest, watch} = require("gulp");
+
+const gulp = require("gulp");
+
+const sass = require("gulp-sass")(require("node-sass"));
+
+const notify = require("gulp-notify");
+
+const image = require("gulp-image");
+
+const concat = require("gulp-concat");
+
+const autoprefixer = require("autoprefixer");
+
+const postcss = require("gulp-postcss");
+
+const cssnano = require("cssnano");
+
+const sourcemaps = require("gulp-sourcemaps");
+
+const terser = require("gulp-terser-js");
+
+const rename = require("gulp-rename");
+
+
+gulp.task('image', (done) => {
+    gulp.src('src/img/**/*')
+      .pipe(image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10
+    }))
+      .pipe(gulp.dest('./build/img'))
+      .pipe(notify({message: "minified image"}));
+      done();
+     
+  });
+
+  function css() {
+    return src("src/scss/app.scss")
+       .pipe( sourcemaps.init() )
+      .pipe(sass())
+      .pipe( postcss( [autoprefixer(), cssnano() ]) )
+      .pipe( sourcemaps.write(".") )
+      .pipe(dest("./build/css"));
+  }
+
+function javascript(){
+       return src("src/js/**/*.js")
+     .pipe(sourcemaps.init())
+     .pipe(concat("bundle.js"))
+     .pipe(terser())
+     .pipe(sourcemaps.write("."))
+     .pipe(rename({suffix: ".min"}))
+     .pipe(dest("./build/js"))
+   }
+
+  function watchfiles() {
+    watch("src/scss/**/*.scss", css); 
+    watch("src/js/**/*.js", javascript);
+  }
+
+
+exports.css = css;
+exports.watchfiles = watchfiles;
+exports.default = series( css, watchfiles );
